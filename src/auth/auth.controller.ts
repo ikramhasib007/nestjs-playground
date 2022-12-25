403c31223117417421853933c68f787661ddf7ed
ikramhasib007 <ikramhasib007@gmail.com>
2022-12-25 23:31:58 +0600

    authenticated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthCredentailsDto } from './dto/auth-credentials.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('/signup')
   signUp(@Body() createUserDto: CreateUserDto): Promise<void> {
     return this.authService.signUp(createUserDto);
+  }
+
+  @Post('/signin')
+  signIn(@Body() authCredentialsDto: AuthCredentailsDto): Promise<string> {
+    return this.authService.signIn(authCredentialsDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,13 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcryptjs';
 import hashPassword from 'src/utils/auth/hashPassword';
 import { Repository } from 'typeorm';
+import { AuthCredentailsDto } from './dto/auth-credentials.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 
@@ -29,5 +32,15 @@ export class AuthService {
 
   async signUp(createUserDto: CreateUserDto): Promise<void> {
     return this.createUser(createUserDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentailsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.repository.findOneByOrFail({ username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Unable to login');
+    }
   }
 }
